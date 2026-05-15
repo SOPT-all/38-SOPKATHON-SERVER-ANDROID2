749dff66a5e64926ad159873ba461e18c7e2da24
gichanGim <k.kychan2003@gmail.com>
2026-05-15 15:43:26 +0900

    Initial commit

---
+++ src/main/java/org/sopt/soptkathonandroid2/SoptkathonAndroid2Application.java
@@ -0,0 +1,13 @@
+package org.sopt.soptkathonandroid2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoptkathonAndroid2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoptkathonAndroid2Application.class, args);
+    }
+
+}
