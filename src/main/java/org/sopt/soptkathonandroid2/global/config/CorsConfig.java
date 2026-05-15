29d182f0b3947255aef56513bdc00e0b4cd7b0e3
gichanGim <k.kychan2003@gmail.com>
2026-05-15 16:37:44 +0900

    chore: 공통 Config 작성

---
+++ src/main/java/org/sopt/soptkathonandroid2/global/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package org.sopt.soptkathonandroid2.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://fe-domain.p-e.kr"
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization", "Content-Type", "Cache-Control")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
