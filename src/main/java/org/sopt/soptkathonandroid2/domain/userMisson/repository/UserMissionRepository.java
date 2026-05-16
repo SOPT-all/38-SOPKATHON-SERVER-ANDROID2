d3b1a7fe8a0393d40788639367d2778722611d6b
gichanGim <k.kychan2003@gmail.com>
2026-05-17 05:20:07 +0900

    feat: findAllByUserIdAndStatus, findByUserIdAndMissionId 메서드 작성

--- src/main/java/org/sopt/soptkathonandroid2/domain/userMisson/repository/UserMissionRepository.java
+++ src/main/java/org/sopt/soptkathonandroid2/domain/userMisson/repository/UserMissionRepository.java
@@ -1,9 +1,15 @@
 package org.sopt.soptkathonandroid2.domain.userMisson.repository;
 
+import org.sopt.soptkathonandroid2.domain.userMisson.entity.MissionStatus;
 import org.sopt.soptkathonandroid2.domain.userMisson.entity.UserMission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserMissionRepository extends JpaRepository<UserMission, Long> {
+    List<UserMission> findAllByUserIdAndStatus(Long userId, MissionStatus status);
+    Optional<UserMission> findByUserIdAndMissionId(Long userId, Long missionId);
 }
